d167a510ba423997c74eee69cf13630fb66ac9bd
lucax13 <lucas13lima2006@gmail.com>
2025-06-05 11:57:40 -0300

    Create 02-listando-com-strings.js

---
+++ 02-listando-com-strings.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const nome ="Noia";
+const sobrenome ="Noiado";
+let idade = 20;
+let bairro = "Itaquera";
+let estado = "SP";
+
+//concatenação
+let mensagem1 = "Meu nome é "+nome+" "+sobrenome+", tenho "+idade+" anos e moro em"+bairro+"/"+estado+"."; 
+
+console.log(mensagem1);
+// template string/literal
